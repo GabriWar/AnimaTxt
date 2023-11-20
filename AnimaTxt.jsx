fc039edfe2faf34f850088a552b2023fa8d2d21e
Gabriel Duarte Guerra <72227489+GabriWar@users.noreply.github.com>
2023-11-20 10:10:12 -0300

    now its able to change between 2 words
    such logic

--- AnimaTxt.jsx
+++ AnimaTxt.jsx
@@ -1,24 +1,27 @@
-const animateonmouseover = (id, originText) => {
-    if (!originText) {
-        originText = document.getElementById(id).innerText;
+const animateonmouseover = (id, FirstText, SecoundText) => {
+
+    if ((FirstText == undefined)|| (SecoundText == undefined)) {
+        FirstText = document.getElementById(id).innerText;
+        SecoundText = document.getElementById(id).innerText;
     }
     const element = document.getElementById(id);
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let interval = null;
-
     let iteration = 0;
+    let buffer = FirstText;
+    if (document.getElementById(id).innerText == buffer) {buffer = SecoundText}
     clearInterval(interval);
     interval = setInterval(() => {
-        element.innerText = originText
+        element.innerText = buffer
             .split("")
             .map((letter, index) => {
                 if (index < iteration) {
-                    return originText[index];
+                    return buffer[index];
                 }
                 return letters[Math.floor(Math.random() * 26)];
             })
             .join("");
-        if (iteration >= originText.length) {
+        if (iteration >= buffer.length) {
             clearInterval(interval);
         }
         iteration += 1 / 3;
