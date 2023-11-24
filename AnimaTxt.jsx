c165b812ab0e365de6dd3097078e46bf0aaac1ab
Gabriel Duarte Guerra <72227489+GabriWar@users.noreply.github.com>
2023-11-24 08:54:12 -0300

    removed some dumb comments

--- AnimaTxt.jsx
+++ AnimaTxt.jsx
@@ -1,25 +1,26 @@
 const animateonmouseover = (id, FirstText, SecoundText) => {
 
-    if ((FirstText == undefined) || (SecoundText == undefined)) { //check if the user passed the text to be animated, if now we just use the doc innerText
-        if (FirstText != undefined) {SecoundText = FirstText ;}else //if the user passed the first text, we use the doc innerText for the secound text
+    if ((FirstText == undefined) || (SecoundText == undefined)) { 
+        if (FirstText != undefined) {SecoundText = FirstText ;}else 
         FirstText = document.getElementById(id).innerText;
         SecoundText = document.getElementById(id).innerText;
-    } 
-    const element = document.getElementById(id); //saving the innertext in a var
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //all the letters that we can use to animate, should be 26
+    } //surely one of the if statements of all time
+    const element = document.getElementById(id); 
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+    const charnum = 26; 
     let interval = null; 
     let iteration = 0;
-    let buffer = FirstText; //we start by buffering the first text, so we can go back to it later
-    if (document.getElementById(id).innerText == buffer) {buffer = SecoundText} //if the text in the document is the same as the first text, we change the buffer to the secound text
+    let buffer = FirstText; 
+    if (document.getElementById(id).innerText == buffer) {buffer = SecoundText}
     clearInterval(interval);
-    interval = setInterval(() => { //magic happens here
+    interval = setInterval(() => { 
         element.innerText = buffer
             .split("")
             .map((letter, index) => {
                 if (index < iteration) {
                     return buffer[index];
                 }
-                return letters[Math.floor(Math.random() * 26)]; // 26 change to the number of chars in the letters var to change the letters
+                return letters[Math.floor(Math.random() * charnum)]; 
             })
             .join("");
         if (iteration >= buffer.length) {
